9c22fccb679fa7a7c98f0d1687eaafdcab7e23df
ocaksen <ocaksen@gmail.com>
2026-04-05 03:03:51 +0300

    fix: CSP unsafe-eval kaldırıldı

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com",
+      "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https://images.unsplash.com https://randomuser.me https://www.google-analytics.com",
